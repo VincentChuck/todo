9dd906240c1f8a5acb59a72f71a94eaadce37946
Vincent Chuck <vincent.chuck7@gmail.com>
2023-03-05 15:58:53 +0800

    Handle toggle and delete in frontend

--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,4 +1,5 @@
 import type { Todo } from "~/types";
+import { api } from "~/utils/api";
 
 type TodoProps = {
   todo: Todo;
@@ -6,6 +7,20 @@ type TodoProps = {
 
 export default function Todo({ todo }: TodoProps) {
   const { id, text, done } = todo;
+
+  const trpc = api.useContext();
+
+  const { mutate: doneMutation } = api.todo.toggle.useMutation({
+    onSettled: async () => {
+      await trpc.todo.all.invalidate();
+    },
+  });
+
+  const { mutate: deleteMutation } = api.todo.delete.useMutation({
+    onSettled: async () => {
+      await trpc.todo.all.invalidate();
+    },
+  });
 
   return (
     <div className="flex items-center justify-between gap-2">
@@ -16,6 +31,9 @@ export default function Todo({ todo }: TodoProps) {
           name="done"
           id={id}
           checked={done}
+          onChange={(e) => {
+            doneMutation({ id, done: e.target.checked });
+          }}
         />
         <label
           htmlFor={id}
@@ -24,7 +42,12 @@ export default function Todo({ todo }: TodoProps) {
           {text}
         </label>
       </div>
-      <button className="w-auto rounded-lg bg-blue-700 px-2 py-1 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <button
+        className="w-auto rounded-lg bg-blue-700 px-2 py-1 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={() => {
+          deleteMutation(id);
+        }}
+      >
         Delete
       </button>
     </div>
